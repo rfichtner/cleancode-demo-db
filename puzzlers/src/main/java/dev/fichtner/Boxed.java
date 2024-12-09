8869324b6934f1b2baf1cc94bcedaa6a61879e4f
Richard Fichtner <r.fichtner@xdev-software.de>
2024-12-09 15:21:05 +0100

    additional examples

---
+++ puzzlers/src/main/java/dev/fichtner/Boxed.java
@@ -0,0 +1,14 @@
+package dev.fichtner;
+
+public class Boxed {
+
+	public static void main(final String[] args) {
+
+		Integer alice = 42;
+		Integer bob = 42;
+
+		System.out.println(alice == bob);
+
+	}
+
+}
---
+++ puzzlers/src/main/java/dev/fichtner/CrazyJavaDate.java
@@ -0,0 +1,17 @@
+package dev.fichtner;
+
+import java.util.Date;
+
+public class CrazyJavaDate {
+
+	public static void main(final String[] args) {
+
+		Date someDate = new Date(2024,12,9);
+		System.out.println(someDate);
+
+
+	}
+
+}
+	
+
